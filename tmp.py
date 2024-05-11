ef12c6e3241a5e40a5985576fdd8f4db86993644
yosmont <yann.osmont@epitech.eu>
2024-05-11 21:12:28 +0200

    starting a test script

--- tmp.py
+++ tmp.py
@@ -1,9 +1,15 @@
 from gen_vlc_playlist import Playlist, Videos
+import os
 
 if __name__ = "__main__":
-    playlist: Playlist = Playlist()
     videos: Videos = Videos()
     playlistFolder: str = ""
     playlistFolderList: dict[str, list[str]] = {
         "test" : ["test/"]
     }
+    for name, folderList in playlistFolder.items():
+        playlist: Playlist = Playlist()
+        files: list[str] = []
+        for folderpath in folderList:
+            os.chdir(folderpath)
+            files += videos.get_videos_with_subdir()
